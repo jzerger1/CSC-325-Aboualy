5407a1a29336c16ede62b5ac3a7d42599f354256
jzerger1 <jzerger1@murraystate.edu>
2025-01-24 14:03:20 -0600

    Just a simple hello world program to practice pushing things to GitHub. I intend to make something more complicated.

---
+++ GitHubTest2.java
@@ -0,0 +1,7 @@
+//I made a second file to practice using and learning GitHub.
+
+public class GitHubTest2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
